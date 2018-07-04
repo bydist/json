7fd4d40950ff0db698211ce38d6d0c65331daa47
David Callu <callu.david@gmail.com>
2018-07-04 14:54:56 +0200

    feat(cmake): add id and CMakeLists.txt

---
+++ id.cmake
@@ -0,0 +1,14 @@
+set(package json)
+set(version 3.1.2)
+
+byd__package__info(${package}
+    MAINTAINER_NAME "David Callu"
+    MAINTAINER_EMAIL "callu.david@gmail.com"
+    VERSION ${version}-1
+    ABI 3.1
+    )
+
+byd__package__download_info(${package}
+    URL "https://github.com/nlohmann/json/archive/v${version}.tar.gz"
+    URL_HASH SHA1=7b668b6af9839c7a46449a0d7f3389982c21ccbb
+    )
